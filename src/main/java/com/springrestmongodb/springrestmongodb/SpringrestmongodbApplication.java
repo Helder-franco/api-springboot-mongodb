13035a83b3792cfd7a49bbf7727d911f0d96cba8
Helder Franco <helderfranco2011@gmail.com>
2023-04-22 19:28:41 -0300

    primeiro commit - projeto criado

---
+++ src/main/java/com/springrestmongodb/springrestmongodb/SpringrestmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.springrestmongodb.springrestmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrestmongodb/springrestmongodb/SpringrestmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrestmongodb.springrestmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
